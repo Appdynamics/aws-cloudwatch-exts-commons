f9fb64093cc55199f8d5ab9715bfcb9a35467749
Aditya Jagtiani <aditya.jagtiani@ajagtiani-mac.corp.appdynamics.com>
2017-11-27 10:10:58 -0800

    Added utility methods for tests to ensure that none of the access keys get checked in.

--- src/main/java/com/appdynamics/extensions/aws/util/AWSUtil.java
+++ src/main/java/com/appdynamics/extensions/aws/util/AWSUtil.java
@@ -1,7 +1,12 @@
 package com.appdynamics.extensions.aws.util;
 
 import java.io.File;
+import java.io.FilenameFilter;
+import java.util.List;
+import java.util.Map;
 
+import com.appdynamics.extensions.yml.YmlReader;
+import com.google.common.collect.Lists;
 import org.apache.commons.lang.StringUtils;
 
 import com.amazonaws.ClientConfiguration;
@@ -16,68 +21,104 @@ import com.singularity.ee.agent.systemagent.api.AManagedMonitor;
 
 /**
  * @author Florencio Sarmiento
- *
  */
 public class AWSUtil {
-    
+
     public static Long convertToLong(Double metricValue) {
-    	Long value = null;
-    	
-    	if (metricValue == null || metricValue < 0) {
-    		value = 0L;
-    		
-    	} else {
-    		value = Math.round(metricValue);
-    	}
-    	
-    	return value;
+        Long value = null;
+
+        if (metricValue == null || metricValue < 0) {
+            value = 0L;
+
+        } else {
+            value = Math.round(metricValue);
+        }
+
+        return value;
     }
-    
+
     public static String resolvePath(String filename) {
-        if(StringUtils.isBlank(filename)){
+        if (StringUtils.isBlank(filename)) {
             return "";
         }
-        
+
         //for absolute paths
-        if(new File(filename).exists()){
+        if (new File(filename).exists()) {
             return filename;
         }
-        
+
         //for relative paths
         File jarPath = PathResolver.resolveDirectory(AManagedMonitor.class);
         String configFileName = String.format("%s%s%s", jarPath, File.separator, filename);
         return configFileName;
     }
-    
-	public static AWSCredentials createAWSCredentials(Account account, 
-			CredentialsDecryptionConfig credentialsDecryptionConfig) {
-		String awsAccessKey = account.getAwsAccessKey();
-		String awsSecretKey = account.getAwsSecretKey();
-		
-		if (credentialsDecryptionConfig != null && 
-				credentialsDecryptionConfig.isDecryptionEnabled()) {
-			Decryptor decryptor = new Decryptor(credentialsDecryptionConfig.getDecryptionKey());
-			awsAccessKey = decryptor.decrypt(awsAccessKey);
-			awsSecretKey = decryptor.decrypt(awsSecretKey);
-		}
-		
-		AWSCredentials awsCredentials = new BasicAWSCredentials(awsAccessKey, awsSecretKey);
-		return awsCredentials;
-	}
-	
-	public static ClientConfiguration createAWSClientConfiguration(int maxErrorRetrySize, 
-			ProxyConfig proxyConfig) {
-		ClientConfiguration awsClientConfig = new ClientConfiguration();
-		awsClientConfig.setMaxErrorRetry(maxErrorRetrySize);
-		
-		if (proxyConfig != null && StringUtils.isNotBlank(proxyConfig.getHost()) &&
-				proxyConfig.getPort() != null) {
-			awsClientConfig.setProxyHost(proxyConfig.getHost());
-			awsClientConfig.setProxyPort(proxyConfig.getPort());
-			awsClientConfig.setProxyUsername(proxyConfig.getUsername());
-			awsClientConfig.setProxyPassword(proxyConfig.getPassword());
-		}
-		
-		return awsClientConfig;
-	}
+
+    public static AWSCredentials createAWSCredentials(Account account,
+                                                      CredentialsDecryptionConfig credentialsDecryptionConfig) {
+        String awsAccessKey = account.getAwsAccessKey();
+        String awsSecretKey = account.getAwsSecretKey();
+
+        if (credentialsDecryptionConfig != null &&
+                credentialsDecryptionConfig.isDecryptionEnabled()) {
+            Decryptor decryptor = new Decryptor(credentialsDecryptionConfig.getDecryptionKey());
+            awsAccessKey = decryptor.decrypt(awsAccessKey);
+            awsSecretKey = decryptor.decrypt(awsSecretKey);
+        }
+
+        AWSCredentials awsCredentials = new BasicAWSCredentials(awsAccessKey, awsSecretKey);
+        return awsCredentials;
+    }
+
+    public static ClientConfiguration createAWSClientConfiguration(int maxErrorRetrySize,
+                                                                   ProxyConfig proxyConfig) {
+        ClientConfiguration awsClientConfig = new ClientConfiguration();
+        awsClientConfig.setMaxErrorRetry(maxErrorRetrySize);
+
+        if (proxyConfig != null && StringUtils.isNotBlank(proxyConfig.getHost()) &&
+                proxyConfig.getPort() != null) {
+            awsClientConfig.setProxyHost(proxyConfig.getHost());
+            awsClientConfig.setProxyPort(proxyConfig.getPort());
+            awsClientConfig.setProxyUsername(proxyConfig.getUsername());
+            awsClientConfig.setProxyPassword(proxyConfig.getPassword());
+        }
+
+        return awsClientConfig;
+    }
+
+    public static List<String> getAWSSecretKeysFromCfg(File[] configFiles) {
+        List<String> secretKeysFromCfg = Lists.newArrayList();
+        for (File configFile : configFiles) {
+            Map<String, ?> config = YmlReader.readFromFile(configFile);
+            List<Map> accounts = (List) config.get("accounts");
+            if (accounts != null) {
+                for (Map account : accounts) {
+                    secretKeysFromCfg.add(account.get("awsSecretKey").toString());
+                }
+            }
+        }
+        return secretKeysFromCfg;
+    }
+
+    public static List<String> getAWSAccessKeysFromCfg(File[] configFiles) {
+        List<String> accessKeysFromCfg = Lists.newArrayList();
+        for (File configFile : configFiles) {
+            Map<String, ?> config = YmlReader.readFromFile(configFile);
+            List<Map> accounts = (List) config.get("accounts");
+            if (accounts != null) {
+                for (Map account : accounts) {
+                    accessKeysFromCfg.add(account.get("awsAccessKey").toString());
+                }
+            }
+        }
+        return accessKeysFromCfg;
+    }
+
+    public static File[] getConfigFilesFromDir(String dirName) {
+        File dir = new File(dirName);
+        return dir.listFiles(new FilenameFilter() {
+            public boolean accept(File dir, String filename) {
+                return filename.endsWith(".yaml") || filename.endsWith(".yml");
+            }
+        });
+    }
 }
