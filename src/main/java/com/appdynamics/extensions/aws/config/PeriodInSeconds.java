bf35560effefb6e58e150b0b07dd7f1cf72d3bfd
VishakaSekar <vishaka.sekar@appdynamics.com>
2018-10-09 14:30:36 -0700

    adding periodInSeconds to config yml

---
+++ src/main/java/com/appdynamics/extensions/aws/config/PeriodInSeconds.java
@@ -0,0 +1,14 @@
+package com.appdynamics.extensions.aws.config;
+
+public class PeriodInSeconds {
+
+    public int getPeriodInSeconds() {
+        return periodInSeconds;
+    }
+
+    public void setPeriodInSeconds(int periodInSeconds) {
+        this.periodInSeconds = periodInSeconds;
+    }
+
+    private int periodInSeconds;
+}
